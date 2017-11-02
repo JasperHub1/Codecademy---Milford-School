733296bbd645fe66c3b4bb13e77ad24053e446b1
JasperHub1 <31580595+JasperHub1@users.noreply.github.com>
2017-11-02 16:29:40 +1100

    re-write everything from scratch
    re-write everything from scratch

--- js/main.js
+++ js/main.js
@@ -1,27 +1,27 @@
 
-$(document).ready(() => {
-  $('.glyphicon-star').on('click', event => {
-    $(event.currentTarget).toggleClass('active');
-  })
-  $('.glyphicon-remove').on('click', event => {
-    $(event.currentTarget).parent().remove();
-  })
+// $(document).ready(() => {
+//   $('.glyphicon-star').on('click', event => {
+//     $(event.currentTarget).toggleClass('active');
+//   })
+//   $('.glyphicon-remove').on('click', event => {
+//     $(event.currentTarget).parent().remove();
+//   })
 
 
-  // use event.preventDefault to disable type="submit" auto refreshing page which will delete any added contents
-  // or change type=submit to button
-  // issue of above two method is the placeholder will not be refreshed to default, so added $('#toDo').val("");
+//   // use event.preventDefault to disable type="submit" auto refreshing page which will delete any added contents
+//   // or change type=submit to button
+//   // issue of above two method is the placeholder will not be refreshed to default, so added $('#toDo').val("");
 
-  $('#addBtn').on('click', event => {
-  	event.preventDefault()
-  	// var text = document.getElementById('toDo').value;
-  	var text = $('#toDo').val();
-  	var html = '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
-  	if (text !== "") {
-  		$(".list").append(html);
-  		$('#toDo').val("");
-  	}
-  });
+//   $('#addBtn').on('click', event => {
+//   	event.preventDefault()
+//   	// var text = document.getElementById('toDo').value;
+//   	var text = $('#toDo').val();
+//   	var html = '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+//   	if (text !== "") {
+//   		$(".list").append(html);
+//   		$('#toDo').val("");
+//   	}
+//   });
 
 
-})
+// })
