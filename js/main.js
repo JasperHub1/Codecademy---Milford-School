1d05487c387bcd5e6a52470101e8e938eb155845
JasperHub1 <31580595+JasperHub1@users.noreply.github.com>
2017-11-02 23:13:15 +1100

    re-write everything from scratch

--- js/main.js
+++ js/main.js
@@ -1,27 +1,17 @@
+$(document).ready(() => {
+	$('.btn').on('click', () => {
+		var emailValue = $('#email-input').val();
+		var passwordValue = $('#password-input').val();
+		
+		if (emailValue == "") {
+			$('.email-error').html("Please enter your email.")
+		}
 
-// $(document).ready(() => {
-//   $('.glyphicon-star').on('click', event => {
-//     $(event.currentTarget).toggleClass('active');
-//   })
-//   $('.glyphicon-remove').on('click', event => {
-//     $(event.currentTarget).parent().remove();
-//   })
+		if (passwordValue == "") {
+			$('.password-error').html("Please enter your password.");
+		}
 
-
-//   // use event.preventDefault to disable type="submit" auto refreshing page which will delete any added contents
-//   // or change type=submit to button
-//   // issue of above two method is the placeholder will not be refreshed to default, so added $('#toDo').val("");
-
-//   $('#addBtn').on('click', event => {
-//   	event.preventDefault()
-//   	// var text = document.getElementById('toDo').value;
-//   	var text = $('#toDo').val();
-//   	var html = '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
-//   	if (text !== "") {
-//   		$(".list").append(html);
-//   		$('#toDo').val("");
-//   	}
-//   });
+	})
 
 
-// })
+})
